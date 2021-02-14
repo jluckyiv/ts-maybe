2c578fc791fc63ac12a81ee7c56b80843fece94a
Jack Lucky <jack.lucky.iv@gmail.com>
2021-02-14 12:11:08 -0800

    Cleaned up tests

--- src/__tests__/maybe.test.ts
+++ src/__tests__/maybe.test.ts
@@ -1,5 +1,14 @@
 import { Maybe, Just, Nothing } from "../maybe"
 
+const toUpper = (s: string) => s.toUpperCase()
+const toString1 = (n: number) => n.toString()
+const toString2 = (n1: number) => (n2: number) => n1.toString() + n2.toString();
+const add1 = (n: number) => n + 1
+const add2 = (n1: number) => (n2: number) => n1 + n2;
+const add3 = (n1: number) => (n2: number) => (n3: number) => n1 + n2 + n3;
+const add4 = (n1: number) => (n2: number) => (n3: number) => (n4: number) => n1 + n2 + n3 + n4;
+const add5 = (n1: number) => (n2: number) => (n3: number) => (n4: number) => (n5: number) => n1 + n2 + n3 + n4 + n5;
+
 test('Maybe.withDefault', () => {
     const maybe = Just(1)
     const actual = Maybe.withDefault(0)(maybe)
@@ -8,60 +17,72 @@ test('Maybe.withDefault', () => {
 
 test('Maybe.map with Nothing', () => {
     const maybe = Nothing
-    const toUpper = (s: string) => s.toUpperCase()
     const actual = Maybe.map(toUpper)(maybe)
     expect(actual).toStrictEqual(Nothing)
 })
 
 test('Maybe.map with int', () => {
     const maybe = Just(1)
-    const addOne = (n: number) => n + 1
-    const actual = Maybe.map(addOne)(maybe)
+    const actual = Maybe.map(add1)(maybe)
     expect(actual).toStrictEqual(Just(2))
 })
 
 test('Maybe.map with string', () => {
     const maybe = Just("hello")
-    const toUpper = (s: string) => s.toUpperCase()
     const actual = Maybe.map(toUpper)(maybe)
     expect(actual).toStrictEqual(Just("HELLO"))
 })
 
 test('Maybe.map int to string', () => {
     const maybe = Just(1)
-    const toString = (n: number) => n.toString()
-    const actual = Maybe.map(toString)(maybe)
-    const expected = Just("1")
-    expect(actual).toStrictEqual(expected)
+    const actual = Maybe.map(toString1)(maybe)
+    expect(actual).toStrictEqual(Just("1"))
 })
 
 test('Maybe.map2 with first Nothing', () => {
-    const m1 = Nothing
-    const m2 = Just(1)
-    const add = (n1: number) => (n2: number) => n1 + n2;
-    const actual = Maybe.map2(add)(m1)(m2)
+    const m1 = Nothing, m2 = Just(1)
+    const actual = Maybe.map2(add2)(m1)(m2)
     expect(actual).toStrictEqual(Nothing)
 })
 
 test('Maybe.map2 with second Nothing', () => {
-    const m1 = Just(1)
-    const m2 = Nothing
-    const add = (n1: number) => (n2: number) => n1 + n2;
-    const actual = Maybe.map2(add)(m1)(m2)
+    const m1 = Just(1), m2 = Nothing
+    const actual = Maybe.map2(add2)(m1)(m2)
     expect(actual).toStrictEqual(Nothing)
 })
+
 test('Maybe.map2 with ints', () => {
-    const m1 = Just(1)
-    const m2 = Just(2)
-    const add = (n1: number) => (n2: number) => n1 + n2;
-    const actual = Maybe.map2(add)(m1)(m2)
+    const m1 = Just(1), m2 = Just(2)
+    const actual = Maybe.map2(add2)(m1)(m2)
     expect(actual).toStrictEqual(Just(3))
 })
 
 test('Maybe.map2 int to string', () => {
-    const m1 = Just(1)
-    const m2 = Just(2)
-    const toString = (n1: number) => (n2: number) => n1.toString() + n2.toString();
-    const actual = Maybe.map2(toString)(m1)(m2)
+    const m1 = Just(1), m2 = Just(2)
+    const actual = Maybe.map2(toString2)(m1)(m2)
     expect(actual).toStrictEqual(Just("12"))
+})
+
+test('Maybe.map3 with first Nothing', () => {
+    const m1 = Nothing, m2 = Just(2), m3 = Just(3)
+    const actual = Maybe.map3(add3)(m1)(m2)(m3)
+    expect(actual).toStrictEqual(Nothing)
+})
+
+test('Maybe.map3 with second Nothing', () => {
+    const m1 = Just(1), m2 = Nothing, m3 = Just(3)
+    const actual = Maybe.map3(add3)(m1)(m2)(m3)
+    expect(actual).toStrictEqual(Nothing)
+})
+
+test('Maybe.map3 with third Nothing', () => {
+    const m1 = Just(1), m2 = Just(2), m3 = Nothing
+    const actual = Maybe.map3(add3)(m1)(m2)(m3)
+    expect(actual).toStrictEqual(Nothing)
+})
+
+test('Maybe.map3 with ints', () => {
+    const m1 = Just(1), m2 = Just(2), m3 = Just(3)
+    const actual = Maybe.map3(add3)(m1)(m2)(m3)
+    expect(actual).toStrictEqual(Just(6))
 })
--- src/maybe.ts
+++ src/maybe.ts
@@ -40,10 +40,19 @@ export const map2 = <A, B, C>(fun: (arg1: A) => (arg2: B) => C) => (maybeA: Mayb
     }
 }
 
+export const map3 = <A, B, C, D>(fun: (arg1: A) => (arg2: B) => (arg3: C) => D) => (maybeA: Maybe<A>) => (maybeB: Maybe<B>) => (maybeC: Maybe<C>): Maybe<D> => {
+    if (maybeA.kind == "Just" && maybeB.kind == "Just" && maybeC.kind == "Just") {
+        return Just(fun(maybeA.value)(maybeB.value)(maybeC.value))
+    } else {
+        return Nothing
+    }
+}
+
 export const Maybe = {
     withDefault: withDefault,
     map: map,
     map2: map2,
+    map3: map3,
     Just: Just,
     Nothing: Nothing
 }
