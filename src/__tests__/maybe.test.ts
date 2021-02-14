1d18c2d80e038761f3f9469a662dae314dab76e3
Jack Lucky <jack.lucky.iv@gmail.com>
2021-02-14 11:32:40 -0800

    Implement map

--- src/__tests__/maybe.test.ts
+++ src/__tests__/maybe.test.ts
@@ -1,6 +1,28 @@
 import { Maybe, Just, Nothing } from "../maybe"
 
 test('Maybe.withDefault', () => {
-    const m = Just(1)
-    expect(Maybe.withDefault(0)(m)).toBe(1);
+    const maybe = Just(1)
+    const actual = Maybe.withDefault(0)(maybe)
+    expect(actual).toBe(1);
+})
+
+test('Maybe.map with int', () => {
+    const maybe = Just(1)
+    const addOne = (n: number) => n + 1
+    const actual = Maybe.map(addOne)(maybe)
+    expect(actual).toStrictEqual(Just(2))
+})
+
+test('Maybe.map with string', () => {
+    const maybe = Just("hello")
+    const toUpper = (s: string) => s.toUpperCase()
+    const actual = Maybe.map(toUpper)(maybe)
+    expect(actual).toStrictEqual(Just("HELLO"))
+})
+
+test('Maybe.map with Nothing', () => {
+    const maybe = Nothing
+    const toUpper = (s: string) => s.toUpperCase()
+    const actual = Maybe.map(toUpper)(maybe)
+    expect(actual).toStrictEqual(Nothing)
 })
--- src/maybe.ts
+++ src/maybe.ts
@@ -2,15 +2,17 @@ interface Just<T> { kind: "Just"; value: T };
 interface Nothing { kind: "Nothing" };
 type Maybe<T> = Just<T> | Nothing
 
-export const Just = <T>(value: T): Just<T> => {
+const just = <T>(value: T): Just<T> => {
     return { kind: "Just", value };
 }
+export const Just = just
 
-export const Nothing = (): Nothing => {
+const nothing = (): Nothing => {
     return { kind: "Nothing" };
 }
+export const Nothing = nothing()
 
-export const withDefault = <T>(defaultValue: T) => (maybe: Maybe<T>) => {
+export const withDefault = <T>(defaultValue: T) => (maybe: Maybe<T>): T => {
     switch (maybe.kind) {
         case "Just": return maybe.value
         case "Nothing": return defaultValue
@@ -20,26 +22,21 @@ export const withDefault = <T>(defaultValue: T) => (maybe: Maybe<T>) => {
     }
 }
 
+export const map = <T>(fun: (arg: T) => T) => (maybe: Maybe<T>): Maybe<T> => {
+    switch (maybe.kind) {
+        case "Just": return Just(fun(maybe.value))
+        case "Nothing": return Nothing
+        default:
+            const _: never = maybe
+            return _
+    }
+}
+
 export const Maybe = {
     withDefault: withDefault,
+    map: map,
     Just: Just,
     Nothing: Nothing
 }
 
 export default Maybe
-
-/*
-export function fromLeft<L, R, DV>(value: Left<L>, defaultValue: DV): Left<L>;
-export function fromLeft<L, R, DV>(value: Right<R>, defaultValue: DV): DV;
-export function fromLeft<L, R, DV>(value: Either<L, R>, defaultValue: DV): Left<L> | DV;
-export function fromLeft<L, R, DV>(value: Either<L, R>, defaultValue: DV) {
-    return isLeft(value) ? value.left : defaultValue;
-}
-
-export function fromRight<L, R, DV>(value: Right<R>, defaultValue: DV): Right<R>;
-export function fromRight<L, R, DV>(value: Left<L>, defaultValue: DV): DV;
-export function fromRight<L, R, DV>(value: Either<L, R>, defaultValue: DV): Right<R> | DV;
-export function fromRight<L, R, DV>(value: Either<L, R>, defaultValue: DV) {
-    return isRight(value) ? value.right : defaultValue;
-}
-*/
